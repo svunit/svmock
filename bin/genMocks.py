1df0375ea6b9066d8ec9832c6664fb5aac670a4d
neil johnson <nosnhojn@gmail.com>
2018-06-12 13:12:06 -0400

    refactored

--- bin/genMocks.py
+++ bin/genMocks.py
@@ -1,45 +1,46 @@
+def __mocker(numargs):
+  fout = open('../src/__mocker' + str(numargs) + '.sv', 'w+')
+  fout.write ('class __mocker%0d #(\n' % numargs)
+  for j in range(0,numargs):
+    if (j == numargs-1):
+      fout.write ('  type TYPE%d = int)\n' % j)
+    else:
+      fout.write ('  type TYPE%d = int,\n' % j)
+  fout.write ('extends __mocker;\n')
+  fout.write ('\nfunction new(string name, ref __mocker __mockers[$]);\n')
+  fout.write ('  super.new(name, __mockers);\n')
+  fout.write ('endfunction\n\n')
+  for j in range(0,numargs):
+    fout.write ('TYPE%0d withAct_%0d, withExp_%0d;\n' % (j,j,j))
+  fout.write ('\nfunction void Called(\n')
+  for j in range(0,numargs):
+    if (j == numargs-1):
+      fout.write ('  TYPE%0d ARG%0d);\n' % (j,j))
+    else:
+      fout.write ('  TYPE%0d ARG%0d,\n' % (j,j))
+  fout.write ('timesCnt += 1;\n')
+  for j in range(0,numargs):
+    fout.write('withAct_%0d = ARG%0d;\n' % (j,j))
+  fout.write ('endfunction\n')
 
+  fout.write ('\nfunction void With(\n')
+  for j in range(0,numargs):
+    if (j == numargs-1):
+      fout.write ('  TYPE%0d ARG%0d);\n' % (j,j))
+    else:
+      fout.write ('  TYPE%0d ARG%0d,\n' % (j,j))
+  fout.write ('checkWith = 1;\n')
+  for j in range(0,numargs):
+    fout.write ('withExp_%0d = ARG%0d;\n' % (j,j))
+  fout.write ('endfunction\n')
+  fout.write ('\nfunction bit check();\n')
+  fout.write ('check = super.check();\n')
+  for j in range(0,numargs):
+    fout.write ('check &= (checkWith) ? (withExp_%0d == withAct_%0d)  : 1;\n' % (j,j))
+  fout.write ('return check;\n')
+  fout.write ('endfunction\n')
+  fout.write ('endclass\n')
 
 if __name__ == "__main__":
   for i in range(1,10):
-    fout = open('../src/__mocker' + str(i) + '.sv', 'w+')
-    fout.write ('class __mocker%0d #(\n' % i)
-    for j in range(0,i):
-      if (j == i-1):
-        fout.write ('  type TYPE%d = int)\n' % j)
-      else:
-        fout.write ('  type TYPE%d = int,\n' % j)
-    fout.write ('extends __mocker;\n')
-    fout.write ('\nfunction new(string name, ref __mocker __mockers[$]);\n')
-    fout.write ('  super.new(name, __mockers);\n')
-    fout.write ('endfunction\n\n')
-    for j in range(0,i):
-      fout.write ('TYPE%0d withAct_%0d, withExp_%0d;\n' % (j,j,j))
-    fout.write ('\nfunction void Called(\n')
-    for j in range(0,i):
-      if (j == i-1):
-        fout.write ('  TYPE%0d ARG%0d);\n' % (j,j))
-      else:
-        fout.write ('  TYPE%0d ARG%0d,\n' % (j,j))
-    fout.write ('timesCnt += 1;\n')
-    for j in range(0,i):
-      fout.write('withAct_%0d = ARG%0d;\n' % (j,j))
-    fout.write ('endfunction\n')
-
-    fout.write ('\nfunction void With(\n')
-    for j in range(0,i):
-      if (j == i-1):
-        fout.write ('  TYPE%0d ARG%0d);\n' % (j,j))
-      else:
-        fout.write ('  TYPE%0d ARG%0d,\n' % (j,j))
-    fout.write ('checkWith = 1;\n')
-    for j in range(0,i):
-      fout.write ('withExp_%0d = ARG%0d;\n' % (j,j))
-    fout.write ('endfunction\n')
-    fout.write ('\nfunction bit check();\n')
-    fout.write ('check = super.check();\n')
-    for j in range(0,i):
-      fout.write ('check &= (checkWith) ? (withExp_%0d == withAct_%0d)  : 1;\n' % (j,j))
-    fout.write ('return check;\n')
-    fout.write ('endfunction\n')
-    fout.write ('endclass\n')
+    __mocker(i)
