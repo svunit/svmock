5e347d14c76bf08ca63064775f6ab59812e2ff88
neil johnson <nosnhojn@gmail.com>
2018-07-10 17:55:56 -0400

    update

--- examples/class/basic/flintstones_mock.sv
+++ examples/class/basic/flintstones_mock.sv
@@ -1,5 +1,5 @@
 `SVMOCK(mock_flintstones, flintstones)
-  `SVMOCK_VOIDFUNCTION0(dino)
-  `SVMOCK_FUNCTION2(pebbles, int, int, fred, , string, wilma, [int])
-  `SVMOCK_VOIDFUNCTION1(bam_bam, int, barney, )
+  `SVMOCK_VFUNC0(dino)
+  `SVMOCK_FUNC2(pebbles, int, int, fred, , string, wilma, [int])
+  `SVMOCK_VFUNC1(bam_bam, int, barney, )
 `SVMOCK_END
