32daef1282db953ef0b1acf4a5b4dbb6c01b48a9
neil johnson <nosnhojn@gmail.com>
2018-07-10 13:46:44 -0400

    refactor checkpoint

--- bin/genMocks.py
+++ bin/genMocks.py
@@ -1,3 +1,12 @@
+def mockers(numargs):
+  fout = open('../src/__mocker' + str(numargs) + '.svh', 'w+')
+
+  base_mocker_class(numargs, fout)
+  function_mocker_class(numargs, fout)
+  void_function_mocker_class(numargs, fout)
+  task_mocker_class(numargs, fout)
+
+
 def oneArgString(idx, delim=' '):
   return 'TYPE%0d%sARG%0d%sMOD%d' % (idx,delim,idx,delim,idx)
 
@@ -16,134 +25,6 @@ def functionDecl(name,numargs,type='void'):
 
 def taskDecl(name,numargs):
   return 'task %s(%s);' % (name, allArgString(numargs))
-
-
-def mockers(numargs):
-  fout = open('../src/__mocker' + str(numargs) + '.svh', 'w+')
-
-  # macro header
-  fout.write ('`define SVMOCK_MOCKER_CLASS%0d(NAME,RETURNS%s,MODIFIER=) \\\n' % (numargs, allArgString(numargs, ',', ',')))
-
-  # class & new
-  fout.write ('class __``NAME``MODIFIER``__mocker  extends __mocker; \\\n' +
-              '`PARENT parent; \\\n' +
-              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent); \\\n' +
-              '  super.new(name, __mockers); \\\n' +
-              '  parent = _parent; \\\n' +
-              'endfunction \\\n')
-
-  # with comparison properties
-  for j in range(0,numargs):
-    fout.write ('TYPE%0d withAct_%0d MOD%0d, withExp_%0d MOD%0d; \\\n' % (j,j,j,j,j))
-
-  # Called
-  fout.write (functionDecl('called',numargs) + ' \\\n')
-  fout.write ('  timesCnt += 1; \\\n')
-  for j in range(0,numargs):
-    fout.write('  withAct_%0d = ARG%0d; \\\n' % (j,j))
-  fout.write ('endfunction \\\n')
-
-  # With
-  fout.write (functionDecl('with_args',numargs) + ' \\\n')
-  fout.write ('  checkWith = 1; \\\n')
-  for j in range(0,numargs):
-    fout.write ('  withExp_%0d = ARG%0d; \\\n' % (j,j))
-  fout.write ('endfunction \\\n')
-
-  # check
-  fout.write ('function bit check(); \\\n')
-  fout.write ('  check = super.check(); \\\n')
-  for j in range(0,numargs):
-    fout.write ('  check &= (checkWith) ? (withExp_%0d == withAct_%0d)  : 1; \\\n' % (j,j))
-  fout.write ('  return check; \\\n')
-  fout.write ('endfunction \\\n')
-
-  fout.write ('endclass\n\n')
-
-  # class & new
-  fout.write ('`define SVMOCK_FUNCTION_MOCKER_CLASS%0d(NAME,RETURNS%s) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
-              '`SVMOCK_MOCKER_CLASS%0d(NAME,RETURNS%s,_base) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
-              'class __``NAME``__mocker  extends __``NAME``_base__mocker; \\\n' +
-
-              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent, input __``NAME``__mocker associate = null); \\\n' +
-              '  super.new(name, __mockers, _parent); \\\n' +
-              '  if (associate != null) associate.possibilities[name] = this; \\\n' +
-              'endfunction \\\n' +
-
-              'virtual ' + functionDecl('NAME',numargs,'RETURNS') + ' \\\n' +     # NAME
-              'endfunction \\\n' +
-
-              'RETURNS returnsVal; \\\n' +
-              'function void returns(RETURNS r); \\\n' +                          # returns
-              '  overrideReturn = 1; \\\n' +
-              '  returnsVal = r; \\\n' +
-              'endfunction \\\n' +
-
-              '__``NAME``__mocker possibilities [string]; \\\n' +
-              '__``NAME``__mocker instead; \\\n' +
-              'function void will_by_default(string i); \\\n' +                   # will_by_default
-              '  instead = possibilities[i]; \\\n' +
-              'endfunction \\\n' +
-
-              'function void clear(); \\\n' +                                     # clear
-              '  super.clear(); \\\n' +
-              '  instead = null; \\\n' +
-              'endfunction \\\n' +
-
-              'endclass\n\n')
-
-  fout.write ('`define SVMOCK_VOID_FUNCTION_MOCKER_CLASS%0d(NAME%s) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
-              '`SVMOCK_MOCKER_CLASS%0d(NAME,RETURNS%s,_base) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
-              'class __``NAME``__mocker  extends __``NAME``_base__mocker; \\\n' +
-
-              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent, input __``NAME``__mocker associate = null); \\\n' +
-              '  super.new(name, __mockers, _parent); \\\n' +
-              '  if (associate != null) associate.possibilities[name] = this; \\\n' +
-              'endfunction \\\n' +
-
-              'virtual ' + functionDecl('NAME',numargs) + ' \\\n' +              # NAME
-              'endfunction \\\n' +
-
-              '__``NAME``__mocker possibilities [string]; \\\n' +                # will_by_default
-              '__``NAME``__mocker instead; \\\n' +
-              'function void will_by_default(string i); \\\n' +
-              '  instead = possibilities[i]; \\\n' +
-              'endfunction \\\n' +
-
-              'function void clear(); \\\n' +                                    # clear
-              '  super.clear(); \\\n' +
-              '  instead = null; \\\n' +
-              'endfunction \\\n' +
-
-              'endclass\n\n')
-
-  fout.write ('`define SVMOCK_TASK_MOCKER_CLASS%0d(NAME%s) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
-              '`SVMOCK_MOCKER_CLASS%0d(NAME,void%s,_base) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
-              'class __``NAME``__mocker  extends __``NAME``_base__mocker; \\\n' +
-
-              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent, input __``NAME``__mocker associate = null); \\\n' +
-              '  super.new(name, __mockers, _parent); \\\n' +
-              '  if (associate != null) associate.possibilities[name] = this; \\\n' +
-              'endfunction \\\n' +
-
-
-              'virtual ' + taskDecl('NAME',numargs) + ' \\\n' +                  # NAME
-              'endtask \\\n' +
-
-              '__``NAME``__mocker possibilities [string]; \\\n' +                # will_by_default
-              '__``NAME``__mocker instead; \\\n' +
-              'function void will_by_default(string i); \\\n' +
-              '  instead = possibilities[i]; \\\n' +
-              'endfunction \\\n' +
-
-              'function void clear(); \\\n' +                                    # clear
-              '  super.clear(); \\\n' +
-              '  instead = null; \\\n' +
-              'endfunction \\\n' +
-
-              'endclass\n')
-
-
 
 def method_macros(numargs, fout, type="NORMAL"):
   if (type == "NORMAL"):
@@ -290,6 +171,140 @@ def method_macros(numargs, fout, type="NORMAL"):
                 '    parent.INSTEAD(`args%0d_``ORIGINAL); \\\n' % numargs +
                 '  endtask \\\n' +
                 'endclass\n\n')
+
+def with_comparison_properties(numargs):
+  ret = ''
+  for j in range(0,numargs):
+    ret += 'TYPE%0d withAct_%0d MOD%0d, withExp_%0d MOD%0d; \\\n' % (j,j,j,j,j)
+  return ret
+
+def with_property_assignments(numargs, type='Act'):
+  ret = ''
+  for j in range(0,numargs):
+    ret += '  with%s_%0d = ARG%0d; \\\n' % (type,j,j)
+  return ret
+
+def with_property_check(numargs):
+  ret = ''
+  for j in range(0,numargs):
+    ret += '  check &= (checkWith) ? (withExp_%0d == withAct_%0d)  : 1; \\\n' % (j,j)
+  return ret
+
+def base_mocker_class(numargs, fout):
+  # macro header
+  fout.write ('`define SVMOCK_MOCKER_CLASS%0d(NAME,RETURNS%s,MODIFIER=) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
+              'class __``NAME``MODIFIER``__mocker  extends __mocker; \\\n' +
+
+              '`PARENT parent; \\\n' +
+              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent); \\\n' +
+              '  super.new(name, __mockers); \\\n' +
+              '  parent = _parent; \\\n' +
+              'endfunction \\\n' +
+
+              with_comparison_properties(numargs) +
+
+              functionDecl('called',numargs) + ' \\\n' +                   # called
+              '  timesCnt += 1; \\\n' +
+                 with_property_assignments(numargs) +
+              'endfunction \\\n' +
+
+              functionDecl('with_args',numargs) + ' \\\n' +                # with
+              '  checkWith = 1; \\\n' +
+                 with_property_assignments(numargs, 'Exp') +
+              'endfunction \\\n' +
+
+              'function bit check(); \\\n' +                               # check
+              '  check = super.check(); \\\n' +
+                 with_property_check(numargs) +
+              '  return check; \\\n' +
+              'endfunction \\\n' +
+
+              'endclass\n\n')
+
+def function_mocker_class(numargs, fout):
+  fout.write ('`define SVMOCK_FUNCTION_MOCKER_CLASS%0d(NAME,RETURNS%s) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
+              '`SVMOCK_MOCKER_CLASS%0d(NAME,RETURNS%s,_base) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
+              'class __``NAME``__mocker  extends __``NAME``_base__mocker; \\\n' +
+
+              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent, input __``NAME``__mocker associate = null); \\\n' +
+              '  super.new(name, __mockers, _parent); \\\n' +
+              '  if (associate != null) associate.possibilities[name] = this; \\\n' +
+              'endfunction \\\n' +
+
+              'virtual ' + functionDecl('NAME',numargs,'RETURNS') + ' \\\n' +     # NAME
+              'endfunction \\\n' +
+
+              'RETURNS returnsVal; \\\n' +
+              'function void returns(RETURNS r); \\\n' +                          # returns
+              '  overrideReturn = 1; \\\n' +
+              '  returnsVal = r; \\\n' +
+              'endfunction \\\n' +
+
+              '__``NAME``__mocker possibilities [string]; \\\n' +
+              '__``NAME``__mocker instead; \\\n' +
+              'function void will_by_default(string i); \\\n' +                   # will_by_default
+              '  instead = possibilities[i]; \\\n' +
+              'endfunction \\\n' +
+
+              'function void clear(); \\\n' +                                     # clear
+              '  super.clear(); \\\n' +
+              '  instead = null; \\\n' +
+              'endfunction \\\n' +
+
+              'endclass\n\n')
+
+def void_function_mocker_class(numargs, fout):
+  fout.write ('`define SVMOCK_VOID_FUNCTION_MOCKER_CLASS%0d(NAME%s) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
+              '`SVMOCK_MOCKER_CLASS%0d(NAME,RETURNS%s,_base) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
+              'class __``NAME``__mocker  extends __``NAME``_base__mocker; \\\n' +
+
+              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent, input __``NAME``__mocker associate = null); \\\n' +
+              '  super.new(name, __mockers, _parent); \\\n' +
+              '  if (associate != null) associate.possibilities[name] = this; \\\n' +
+              'endfunction \\\n' +
+
+              'virtual ' + functionDecl('NAME',numargs) + ' \\\n' +              # NAME
+              'endfunction \\\n' +
+
+              '__``NAME``__mocker possibilities [string]; \\\n' +                # will_by_default
+              '__``NAME``__mocker instead; \\\n' +
+              'function void will_by_default(string i); \\\n' +
+              '  instead = possibilities[i]; \\\n' +
+              'endfunction \\\n' +
+
+              'function void clear(); \\\n' +                                    # clear
+              '  super.clear(); \\\n' +
+              '  instead = null; \\\n' +
+              'endfunction \\\n' +
+
+              'endclass\n\n')
+
+def task_mocker_class(numargs, fout):
+  fout.write ('`define SVMOCK_TASK_MOCKER_CLASS%0d(NAME%s) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
+              '`SVMOCK_MOCKER_CLASS%0d(NAME,void%s,_base) \\\n' % (numargs, allArgString(numargs, ',', ',')) +
+              'class __``NAME``__mocker  extends __``NAME``_base__mocker; \\\n' +
+
+              'function new(string name, ref __mocker __mockers[$], input `PARENT _parent, input __``NAME``__mocker associate = null); \\\n' +
+              '  super.new(name, __mockers, _parent); \\\n' +
+              '  if (associate != null) associate.possibilities[name] = this; \\\n' +
+              'endfunction \\\n' +
+
+
+              'virtual ' + taskDecl('NAME',numargs) + ' \\\n' +                  # NAME
+              'endtask \\\n' +
+
+              '__``NAME``__mocker possibilities [string]; \\\n' +                # will_by_default
+              '__``NAME``__mocker instead; \\\n' +
+              'function void will_by_default(string i); \\\n' +
+              '  instead = possibilities[i]; \\\n' +
+              'endfunction \\\n' +
+
+              'function void clear(); \\\n' +                                    # clear
+              '  super.clear(); \\\n' +
+              '  instead = null; \\\n' +
+              'endfunction \\\n' +
+
+              'endclass\n')
 
 if __name__ == "__main__":
   f_macros = open('../src/svmock_mocker_defines.svh', 'w+')
